fa769e39782addc4dee84675335dee16d80be1aa
diegojs <diegokickfly@gmail.com>
2019-03-04 09:47:39 -0300

    Commit schedule

---
+++ src/main/java/com/ciandt/poc/h2/demoh2/DemoH2Application.java
@@ -0,0 +1,12 @@
+package com.ciandt.poc.h2.demoh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoH2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoH2Application.class, args);
+	}
+}
---
+++ src/test/java/com/ciandt/poc/h2/demoh2/DemoH2ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ciandt.poc.h2.demoh2;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DemoH2ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
